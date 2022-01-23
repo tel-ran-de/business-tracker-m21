b8f1b48f1187a0d7e884d9bdaa615a30d34b449c
VitalijMakar <vitalij.makarenko.mail@gmail.com>
2022-01-23 22:14:49 +0100

    BT10 deleted unused files

--- src/main/webapp/src/components/Navigation/RouterSwitch.js
+++ src/main/webapp/src/components/Navigation/RouterSwitch.js
@@ -1,14 +1,12 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
 
-import Projects from "../Pages/Projects/Projects"
 import About from "../Pages/About/About"
 
 export default (props) => {
     return (
         <>
             <Routes>
-                <Route path="/" index element={<Projects/>}/>
                 <Route path="/about" element={<About/>}/>
             </Routes>
         </>
