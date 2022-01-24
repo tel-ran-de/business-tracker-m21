babe81e7a5b4058b8b81eb02e6ca4f68653e04d0
VitaliyMakar <93013704+VitaliyMakar@users.noreply.github.com>
2022-01-24 12:18:47 +0100

    Bt-14 (#16)
    app routing

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,18 +1,17 @@
+import './App.css';
 import React from 'react';
 import Navigation from "./components/Navigation";
+import RouterSwitch from "./components/Navigation/RouterSwitch";
 
 
 function App() {
     return (
 
-        <div className="">
+        <div className="container">
             <Navigation/>
+            <RouterSwitch/>
         </div>
     )
 }
 
 export default App;
-
-
-
-
---
+++ src/main/webapp/src/components/Navigation/RouterSwitch.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Routes} from "react-router-dom";
+
+
+export default (props) => {
+    return (
+        <>
+            <Routes>
+
+            </Routes>
+        </>
+    )
+}
