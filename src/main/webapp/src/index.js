b65926628c75cc1024c7ad126d5dc401151f9a82
VitalijMakar <vitalij.makarenko.mail@gmail.com>
2022-01-24 11:15:24 +0100

    cleaned up / code revie

--- src/main/webapp/src/index.js
+++ src/main/webapp/src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
-import {store} from "./store/store";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </Provider>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
--- src/main/webapp/src/store/store.js
+++
@@ -1,6 +0,0 @@
-import {createStore, applyMiddleware} from "redux";
-import {RootReducer} from "./RootReducer";
-import logger from "redux-logger"
-import thunk from "redux-thunk"
-
-export const store = createStore(RootReducer, applyMiddleware(thunk, logger))
