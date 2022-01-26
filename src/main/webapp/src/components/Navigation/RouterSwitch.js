6d7868fabd2dc78139875467d7d2f3887ed460d1
volchitsa21 <volchitsa21@gmail.com>
2022-01-26 10:33:12 +0100

    implemented projects page

--- src/main/webapp/src/components/Navigation/RouterSwitch.js
+++ src/main/webapp/src/components/Navigation/RouterSwitch.js
@@ -2,11 +2,13 @@ import React from "react";
 import {Route, Routes} from "react-router-dom";
 
 import About from "../Pages/About/About"
+import Projects from "../Pages/Projects/Projects";
 
 export default (props) => {
     return (
         <>
             <Routes>
+                <Route path="/" element={<Projects/>}/>
                 <Route path="/about" element={<About/>}/>
             </Routes>
         </>
