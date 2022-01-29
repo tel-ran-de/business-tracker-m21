de50cc7d960989df7f31a0175dc6d5859395f3a1
VitaliyMakar <93013704+VitaliyMakar@users.noreply.github.com>
2022-01-29 23:42:06 +0100

    Update RootReducer.js

--- src/main/webapp/src/store/reducers/RootReducer.js
+++ src/main/webapp/src/store/reducers/RootReducer.js
@@ -1,11 +1,9 @@
 import {combineReducers} from "redux";
 import {ProjectReducer} from "./ProjectReducer";
-import {TaskReducer} from "./TaskReducer";
 import {MemberReducer} from "./MemberReducer";
 
 
 export const RootReducer = combineReducers({
     project: ProjectReducer,
-    task: TaskReducer,
     member: MemberReducer
 })
