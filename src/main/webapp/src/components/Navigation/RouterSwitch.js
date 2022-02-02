7dd7fea59a0a3f033e7ab60be2eaa72b0a162677
VitaliyMakar <93013704+VitaliyMakar@users.noreply.github.com>
2022-02-02 16:35:51 +0100

    BT 27 (#36)

--- src/main/webapp/src/components/Navigation/RouterSwitch.js
+++ src/main/webapp/src/components/Navigation/RouterSwitch.js
@@ -1,15 +1,16 @@
 import React from "react";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 
 import About from "../Pages/About/About"
 import Projects from "../Pages/Projects/Projects";
 import ProjectDetail from "../Pages/Projects/ProjectDetail";
 
 
-export default (props) => {
+export default () => {
     return (
         <>
             <Routes>
+                <Route exact path="/" index element={<Navigate replace to="/projects" />} />
                 <Route path="/projects" element={<Projects/>}/>
                 <Route path="/projects/:projectId" element={<ProjectDetail/>}/>
                 <Route path="/about" element={<About/>}/>
