e322da61f7925a51e98a766443254b5753000eb0
Volchitsa21 <82124671+Volchitsa21@users.noreply.github.com>
2022-02-07 10:53:30 +0100

    Bt-12 404 page (#39)

--- src/main/webapp/src/components/Navigation/RouterSwitch.js
+++ src/main/webapp/src/components/Navigation/RouterSwitch.js
@@ -4,6 +4,7 @@ import {Navigate, Route, Routes} from "react-router-dom";
 import About from "../Pages/About/About"
 import Projects from "../Pages/Projects/Projects";
 import ProjectDetail from "../Pages/Projects/ProjectDetail";
+import Error from "../Pages/Error/Error";
 
 
 export default () => {
@@ -14,6 +15,7 @@ export default () => {
                 <Route path="/projects" element={<Projects/>}/>
                 <Route path="/projects/:projectId" element={<ProjectDetail/>}/>
                 <Route path="/about" element={<About/>}/>
+                <Route path="*" element={<Error/>}/>
             </Routes>
         </>
     )
