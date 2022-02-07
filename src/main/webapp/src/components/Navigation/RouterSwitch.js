b216901cc6b02669ac1bc529e524844f54204a97
volchitsa21 <volchitsa21@gmail.com>
2022-02-07 10:20:50 +0100

    add page 404

--- src/main/webapp/src/components/Navigation/RouterSwitch.js
+++ src/main/webapp/src/components/Navigation/RouterSwitch.js
@@ -3,6 +3,8 @@ import {Route, Routes} from "react-router-dom";
 
 import About from "../Pages/About/About"
 import Projects from "../Pages/Projects/Projects";
+import Error from "../Pages/Error/Error";
+
 
 export default (props) => {
     return (
@@ -10,6 +12,8 @@ export default (props) => {
             <Routes>
                 <Route path="/projects" element={<Projects/>}/>
                 <Route path="/about" element={<About/>}/>
+                <Route path="*" element={<Error/>}/>
+
             </Routes>
         </>
     )
