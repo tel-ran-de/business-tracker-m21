9d348e42f3a5071e596e105c3b38a0abbcc59e4e
VitaliyMakar <93013704+VitaliyMakar@users.noreply.github.com>
2022-02-08 01:18:09 +0100

    BT-17-5 kpi list (#41)

---
+++ src/main/webapp/src/components/Pages/Kpi/KpiDetail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default ({kpi}) => {
+
+    return (
+        <>
+            <li className="list-group-item list-group-item-success text-start">{kpi.kpi}</li>
+        </>
+    )
+}
